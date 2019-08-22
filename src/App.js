8dbd2ea0526f08255c8a5e05ce90aba4ec6c5463
Michael Williams <manicdesigner@gmail.com>
2019-08-22 14:04:10 -0400

    import Button from ScoreChange, use button component for homeTouchdown, homeFieldGoal, awayTouchdown, awayFieldGoal

--- src/App.js
+++ src/App.js
@@ -2,68 +2,64 @@
 import React, { useState } from 'react';
 import './App.css';
 import BottomRow from './BottomRow';
+import Button from './components/ScoreChange';
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, setHomeScore] = useState(0);
-  const [awayScore, setAwayScore] = useState(0);
+	//TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+	const [homeScore, setHomeScore] = useState(0);
+	const [awayScore, setAwayScore] = useState(0);
 
-  const homeTouchDown = () => {
-    setHomeScore(homeScore + 7);
-  };
+	const homeTouchDown = () => {
+		setHomeScore(homeScore + 7);
+	};
 
-  const homeFieldGoal = () => {
-    setHomeScore(homeScore + 3);
-  };
+	const homeFieldGoal = () => {
+		setHomeScore(homeScore + 3);
+	};
 
-  const awayTouchDown = () => {
-    setAwayScore(awayScore + 7);
-  };
+	const awayTouchDown = () => {
+		setAwayScore(awayScore + 7);
+	};
 
-  const awayFieldGoal = () => {
-    setAwayScore(awayScore + 3);
-  };
+	const awayFieldGoal = () => {
+		setAwayScore(awayScore + 3);
+	};
 
-  return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+	return (
+		<div className="container">
+			<section className="scoreboard">
+				<div className="topRow">
+					<div className="home">
+						<h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+						{/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={homeTouchDown} className="homeButtons__touchdown">
-            Home Touchdown
-          </button>
-          <button onClick={homeFieldGoal} className="homeButtons__fieldGoal">
-            Home Field Goal
-          </button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={awayTouchDown} className="awayButtons__touchdown">
-            Away Touchdown
-          </button>
-          <button onClick={awayFieldGoal} className="awayButtons__fieldGoal">
-            Away Field Goal
-          </button>
-        </div>
-      </section>
-    </div>
-  );
+						<div className="home__score">{homeScore}</div>
+					</div>
+					<div className="timer">00:03</div>
+					<div className="away">
+						<h2 className="away__name">Tigers</h2>
+						<div className="away__score">{awayScore}</div>
+					</div>
+				</div>
+				<BottomRow />
+			</section>
+			<section className="buttons">
+				<div className="homeButtons">
+					{/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+
+					<Button onChange={homeTouchDown} classGiven="homeButtons__touchdown" label="Home Touchdown" />
+
+					<Button onChange={homeFieldGoal} classGiven="homeButtons__fieldGoal" label="Home Field Goal" />
+				</div>
+				<div className="awayButtons">
+					<Button onChange={awayTouchDown} classGiven="awayButtons__touchdown" label="Away Touchdown" />
+
+					<Button onChange={awayFieldGoal} classGiven="awayButtons__fieldGoal" label="Away Field Goal" />
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
